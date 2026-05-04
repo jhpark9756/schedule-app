8e282d81d41a13499bc233f704c0fc942072d1b1
jhpark9756 <64886280+jhpark9756@users.noreply.github.com>
2026-05-04 15:23:49 +0900

    Update firebase-config.js 2

--- firebase-config.js
+++ firebase-config.js
@@ -3,15 +3,25 @@
 // 2) 좌측 메뉴 "Realtime Database" → 데이터베이스 만들기 (테스트 모드)
 // 3) 프로젝트 설정 (톱니바퀴) → 일반 → "내 앱" → 웹 앱 추가
 // 4) 표시되는 firebaseConfig 객체의 값들을 그대로 복사해서 아래에 붙여넣기
-window.firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVZan3xTVJvvinLgePys-s41ZkzB5GBTs",
+  authDomain: "schedule-web-87a52.firebaseapp.com",
+  databaseURL: "https://schedule-web-87a52-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "schedule-web-87a52",
+  storageBucket: "schedule-web-87a52.firebasestorage.app",
+  messagingSenderId: "872796379038",
+  appId: "1:872796379038:web:b5db652f9d5ef81ef7288f"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // 관리자 키.
 //   첫 방문 시 https://<배포주소>/?admin=여기에설정한키 로 한 번 접속하면
